e128fa8b3e8d5f9ca8a50f6bb0e1aef76cf7cce7
Shubham patil <shubham.patil@sincrodigital.com>
2025-07-28 13:32:24 +0530

    adding crud operations to movie service

--- movie-service/controller/movie.controller.js
+++ movie-service/controller/movie.controller.js
@@ -16,3 +16,33 @@ exports.addMovie = async (req, res) => {
     await movie.save();
     res.status(201).json(movie);
 };
+
+exports.getMovieById = async (req, res) => {
+    try {
+        const movie = await  Movie.find({title: req.params.id});
+        if(!movie) res.status(404).json({error: "Movie Not Found"});
+        res.json(movie);
+    } catch (e){
+        res.status(400).json({error: "Invalid Id"});
+    }
+}
+
+exports.deleteMovie = async (req, res) => {
+   try{
+       const deleted = await Movie.findByIdAndDelete(req.params.id);
+       if(!deleted) return res.status(404).json({error: "Movie Not Found"});
+       res.json({message: "Movie Deleted Successfully!"});
+   } catch (e) {
+       res.status(400).json({error: "Invalid ID"});
+   }
+}
+
+exports.updateMovie = async (req, res) => {
+    try {
+        const updated = await Movie.findByIdAndUpdate(req.params.id, req.body, { new: true});
+        if(!updated) return res.status(400).json({error: 'Movie Not Found'});
+        res.json(updated);
+    } catch (e) {
+        res.status(400).json({error: 'Invalid Data'});
+    }
+}
